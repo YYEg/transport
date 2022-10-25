c912c4a212a4e58b8722f6bf454809bf02efca22
Egor <yyrkovskii@mail.ru>
2022-10-25 18:06:26 +0800

    you can find Oporniy plan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.optimize import linprog
 
 
 #Проверка открытости/закрытости задачи
@@ -22,11 +21,31 @@ def isOpen(zapac, potreb, matrix):
         changed = np.hstack((changed, np.zeros(len(newZapac)).reshape(len(newZapac), 1)))
         print(newPotreb)
         print(changed)
-        isOpen(newZapac, newPotreb, changed)
+        opPlan(newZapac, newPotreb, changed)
     elif zapSum < potrebSum:
         print("открытая", zapSum, "<", potrebSum)
 
-#def opPlan(zapac, potreb, matrix):
+def opPlan(zapac, potreb, matrix):
+    i, j = 0, 0
+    m, n = len(zapac), len(potreb)
+    oporPlan = np.zeros((m, n), dtype=int)
+    functionValue = 0
+    while ((i < m) and (j < n)):  # повторяем цикл до сходимости метода
+        transported = min(zapac[i], potreb[j])  #берем минимальное значение из Потребностей/Запасов
+        functionValue += matrix[i, j] * min(zapac[i], potreb[j])  # записываем в итоговую функцию элемент трат
+        zapac[i] -= transported  #
+        potreb[j] -= transported  # обновляем векторы a и b
+        oporPlan[i, j] = transported  # добавляем элемент transported в матрицу oporPlan
+
+        if zapac[i] > potreb[j]:  # делаем сдвиги при выполнении условий
+            j += 1
+        elif zapac[i] < potreb[j]:
+            i += 1
+        else:
+            i += 1
+            j += 1
+    return oporPlan
+
 
 
 #----------------------------------------------Меню------------------------------------------------------
