964f3a1a6c1dfc9fdd861efacea73181055c665c
Egor <yyrkovskii@mail.ru>
2022-10-25 17:26:33 +0800

    open/closed task

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+from scipy.optimize import linprog
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Проверка открытости/закрытости задачи
+def isOpen(zapac, potreb, matrix):
+    zapSum = 0
+    potrebSum = 0
+    changed = np.copy(matrix)
+    newPotreb = np.copy(potreb)
+    newZapac = np.copy(zapac)
+    for i in range(len(zap)):
+        zapSum += zap[i]
+    for i in range(len(newPotreb)):
+        potrebSum += newPotreb[i]
+    if zapSum == potrebSum:
+        print("Задача закрытая", zapSum, "=", potrebSum)
+    elif zapSum > potrebSum:
+        print("открытая", zapSum, ">", potrebSum)
+        valuePotreb = zapSum - potrebSum
+        newPotreb = np.hstack((potreb, valuePotreb))
+        changed = np.hstack((changed, np.zeros(len(newZapac)).reshape(len(newZapac), 1)))
+        print(newPotreb)
+        print(changed)
+        isOpen(newZapac, newPotreb, changed)
+    elif zapSum < potrebSum:
+        print("открытая", zapSum, "<", potrebSum)
+
+#def opPlan(zapac, potreb, matrix):
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#----------------------------------------------Меню------------------------------------------------------
+matrix = np.loadtxt("uslovie.txt", dtype=int) #Загрузка матрицы в программу из файла
+zap = np.loadtxt("zapac.txt", dtype=int) #Загрузка запаса в программу из файла
+potreb = np.loadtxt("potreb.txt", dtype=int) #Загрузка потребности в программу из файла
+(row, col) = matrix.shape #Возвращает размерность массива
+isOpen(zap, potreb, matrix)
+print(matrix)
+print(zap)
+print(potreb)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
